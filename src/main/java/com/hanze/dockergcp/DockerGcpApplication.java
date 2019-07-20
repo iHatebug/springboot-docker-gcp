4e005afd9c79ae9ef679163c8d06f1161181fb22
hanze <303709827@qq.com>
2019-07-20 22:51:27 +0800

    init spring boot project

---
+++ src/main/java/com/hanze/dockergcp/DockerGcpApplication.java
@@ -0,0 +1,13 @@
+package com.hanze.dockergcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerGcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerGcpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hanze/dockergcp/DockerGcpApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hanze.dockergcp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DockerGcpApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
